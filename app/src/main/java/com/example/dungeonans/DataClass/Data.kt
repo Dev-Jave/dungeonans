283176dcf5ff3a60b7dc0e1cf9f4c299cb994168
devdejavu <wndud4576@naver.com>
2022-05-14 14:09:12 +0900

    commit for integrate3

--- app/src/main/java/com/example/dungeonans/DataClass/Data.kt
+++ app/src/main/java/com/example/dungeonans/DataClass/Data.kt
@@ -9,3 +9,4 @@ data class AnswerData(var writerProfile : Int, var writerName : String, var writ
 data class PostCommentData(var type : Int, var commentWriteProfile : Int, var commentWriterName : String, var commentWriterNickname : String, var commentWriteTime : String, var commentBody : String, var like : Int)
 data class PostReCommentData(var reCommentWriterProfile : Int, var reCommentWriterName : String, var reCommentWriterNickname : String, var reCommentWriteTime : String, var reCommentBody : String, var reCommentLike : Int)
 data class AskData(var askUserImage : Int, var askUserName : String, var askUserNickname : String, var askPostTitle : String, var askPostBody : String, var askStatusImage : Int, var askPostLikeCount : String, var askCommentCount : String)
+data class SearchProfileData(var profileName : String, var profileMessage : String)
