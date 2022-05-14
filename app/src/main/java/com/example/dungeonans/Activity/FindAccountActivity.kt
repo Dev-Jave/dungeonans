070ba67acb75dc2f6b393cee16887eea3d03f91c
Yongkingg <dydwns8064@naver.com>
2022-05-14 14:05:33 +0900

    Commit for integrate 2

--- app/src/main/java/com/example/dungeonans/Activity/FindAccountActivity.kt
+++ app/src/main/java/com/example/dungeonans/Activity/FindAccountActivity.kt
@@ -2,6 +2,7 @@ package com.example.dungeonans.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dungeonans.Fragment.FindIdFragment
 import com.example.dungeonans.Fragment.FindPwFragment
@@ -14,6 +15,10 @@ class FindAccountActivity : AppCompatActivity() {
         setContentView(R.layout.empty_activity_layout)
 
         val display: String? = intent.getStringExtra("find")
+
+        if (display == "주영") {
+            Log.d("tag","주영이형너무수고했어 ")
+        }
 
         when(display) {
             "id" -> {
@@ -29,6 +34,7 @@ class FindAccountActivity : AppCompatActivity() {
             }
         }
     }
+
 
     fun moveBackPage() {
         val intent = Intent(this, LoginActivity::class.java)
