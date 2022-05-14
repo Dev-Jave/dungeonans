c54342b618584256e17acaf24921ee8a70876b90
devdejavu <wndud4576@naver.com>
2022-05-14 14:18:07 +0900

    commit for integrate5

--- app/src/main/java/com/example/dungeonans/DataClass/Data.kt
+++ app/src/main/java/com/example/dungeonans/DataClass/Data.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class BlogData(var cardViewTitle : String, var cardViewBody : String, var cardViewWriter : String, var cardViewProfile : Int)
 data class CommunityData(var postTitle : String, var postBody : String, var hashtag : String, var likeCount : String, var commentCount : String)
 data class CommentData(var commentBody : String)
-data class SearchData(var stri1: String)
+data class SearchData(var term: String, var timeStamp : String)
 data class AnswerData(var writerProfile : Int, var writerName : String, var writerNickName : String, var writeTime : String, var postBody : String)
 data class PostCommentData(var type : Int, var commentWriteProfile : Int, var commentWriterName : String, var commentWriterNickname : String, var commentWriteTime : String, var commentBody : String, var like : Int)
 data class PostReCommentData(var reCommentWriterProfile : Int, var reCommentWriterName : String, var reCommentWriterNickname : String, var reCommentWriteTime : String, var reCommentBody : String, var reCommentLike : Int)
